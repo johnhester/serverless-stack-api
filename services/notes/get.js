8f9c25236a5ec212f39458ed8cbafb8e16b862b6
John Hester <hesterusmaximus@gmail.com>
2020-12-16 08:31:41 -0600

    adding handler error

--- services/notes/get.js
+++ services/notes/get.js
@@ -1,10 +1,8 @@
 import handler from "./libs/handler-lib";
-import dynamoDb from "./libs/dynamodb-lib";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
 
-// Some faulty code
-dynamoDb.notExist();
-
-export const main = handler(async (event, context) => {
+// Wrong handler function name
+export const main2 = handler(async (event, context) => {
     const params = {
         TableName: process.env.tableName,
         // 'Key' defines the partition key and sort key of the item to be retrieved
@@ -16,7 +14,7 @@ export const main = handler(async (event, context) => {
         }
     };
 
-    const result = await dynamoDb.get(params);
+    const result = await dynamoDbLib.call("get", params);
     if (!result.Item) {
         throw new Error("Item not found.");
     }
