105d8bd2fbf4ed3c975eabb26872593784f19cc7
John Hester <hesterusmaximus@gmail.com>
2020-12-16 07:53:54 -0600

    adding faulty code

--- services/notes/get.js
+++ services/notes/get.js
@@ -5,10 +5,12 @@ export const main = handler(async (event, context) => {
     const params = {
         TableName: process.env.tableName,
         // 'Key' defines the partition key and sort key of the item to be retrieved
+        // - 'userId': Identity Pool identity id of the authenticated user
+        // - 'noteId': path parameter
         Key: {
-            userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
-            // noteId: event.pathParameters.id, // The id of the note from the path
-        },
+            userId: event.requestContext.identity.cognitoIdentityId,
+            // noteId: event.pathParameters.id
+        }
     };
 
     const result = await dynamoDb.get(params);
